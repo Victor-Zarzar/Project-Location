7b2676dbbabb5d3e2a701e1c83d175b6cb3ed199
Victor-Zarzar <victorzarzar58@gmail.com>
2023-02-08 21:48:02 -0300

    Initial Commit

---
+++ Location/LocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationApp.swift
+//  Location
+//
+//  Created by Victor Zarzar on 08/02/23.
+//
+
+import SwiftUI
+
+@main
+struct LocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
